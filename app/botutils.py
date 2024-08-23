5e0dbadf9978528123438ccc52e501fe67e01d74
Francois Poh <fp522@ic.ac.uk>
2024-08-23 14:09:17 +0100

    feat: extended katakana

--- app/botutils.py
+++ app/botutils.py
@@ -42,8 +42,9 @@ def get_view(user: nextcord.User, callback) -> nextcord.ui.view.View:
 async def take_bot_turn(inter: nextcord.Interaction, previous_word: str, played_words: set[str]) -> str:
     await inter.channel.send(f"My turn!")
 
-    kata = translationtools.hiragana_to_katakana(previous_word)
-    words = await translationtools.get_dictionary(previous_word[-1], kata[-1], previous_word, played_words)
+    hira = translationtools.romanji_to_hiragana(previous_word)
+    kata = translationtools.romaji_to_katakana(previous_word)
+    words = await translationtools.get_dictionary(hira[-1], kata[-1], previous_word, played_words)
     if not words:
         return ""
 
@@ -53,7 +54,7 @@ async def take_bot_turn(inter: nextcord.Interaction, previous_word: str, played_
             break
         match = words[word][i]
         await inter.channel.send(f"{match['word']} ({word}):\n> {', '.join(match['meanings'])}")
-    return word
+    return kata
 
 
 async def take_user_turn(
@@ -81,18 +82,18 @@ async def take_user_turn(
         await inter.channel.send(f"{current.mention} took too long to respond. You lose!")
         return False, ""
 
-    hiragana = translationtools.romaji_to_hiragana(response)
+    hiragana = translationtools.romanji_to_hiragana(response)
+    katakana = translationtools.romaji_to_katakana(response)
 
     logger.info(f"{current.display_name} played {response}")
 
     async def invalid_word(reason: str):
-        await lose_life(f"{hiragana} {reason}")
+        await lose_life(f"{hiragana if hiragana else katakana} {reason}")
         return False
 
-    if not await check_valid_word(hiragana, previous_word, played_words, invalid_word):
+    if not await check_valid_word(katakana, previous_word, played_words, invalid_word):
         return True, ""
 
-    katakana = translationtools.hiragana_to_katakana(hiragana)
     words = await translationtools.get_dictionary(hiragana, katakana, previous_word, played_words)
 
     logger.info(f"checking for {hiragana} or {katakana} in {words.keys()}")
@@ -109,17 +110,16 @@ async def take_user_turn(
         match = matches[i]
         kanji = match['word'] or katakana
         await inter.channel.send(f"{kanji} ({hiragana}):\n> {', '.join(match['meanings'])}")
-    return True, hiragana
+    return True, katakana
 
 
-async def check_valid_word(hiragana: str, previous_word: str, played_words: set[str], invalid_word) -> bool:
-
-    if not hiragana:
+async def check_valid_word(katakana: str, previous_word: str, played_words: set[str], invalid_word) -> bool:
+    if not katakana:
         return await invalid_word("is not a valid Romaji word!")
-    if hiragana in played_words:
+    if katakana in played_words:
         return await invalid_word("has already been played!")
-    if not translationtools.match_kana(previous_word, hiragana):
+    if not translationtools.match_kana(previous_word, katakana):
         return await invalid_word("does not match the previous word!")
-    if hiragana[-1] == 'ん':
+    if katakana[-1] == 'ン':
         return await invalid_word("ends with ん!")
     return True
--- app/translationtools.py
+++ app/translationtools.py
@@ -4,17 +4,19 @@ from jisho_api.word import Word
 def match_kana(prev: str, curr: str) -> bool:
     if not prev:
         return True
-    p = normalise_hiragana(prev)
-    c = normalise_hiragana(curr)
+    p = normalise_katakana(prev)
+    c = normalise_katakana(curr)
     for i in range(min(len(p), len(c))):
-        if p[len(p)-1-i:] == c[:i+1]:
+        if p[len(p) - 1 - i:] == c[:i + 1]:
             return True
     return False
 
 
-def normalise_hiragana(hiragana: str) -> str:
+def normalise_katakana(hiragana: str) -> str:
     normal_map = {
-        'ぢ': 'じ', 'づ': 'ず', 'ゃ': 'や', 'ゅ': 'ゆ', 'ょ': 'よ'
+        'ヂ': 'ジ', 'ヅ': 'ズ',
+        'ャ': 'ヤ', 'ュ': 'ユ', 'ョ': 'ヨ',
+        'ァ': 'ア', 'ィ': 'イ', 'ゥ': 'ウ', 'ェ': 'エ', 'ォ': 'オ'
     }
     return ''.join(normal_map.get(c, c) for c in hiragana)
 
@@ -28,7 +30,7 @@ async def get_dictionary(hira: str, kata: str, previous_word: str, played_words:
     :param played_words: Played words
     :return: Dictionary from readings to words
     """
-    wr1 = Word.request(hira)
+    wr1 = Word.request(hira) if hira else None
     wr2 = Word.request(kata)
     if not wr1 and not wr2:
         return {}
@@ -54,53 +56,62 @@ async def get_dictionary(hira: str, kata: str, previous_word: str, played_words:
     return words
 
 
-def romaji_to_hiragana(word: str) -> str or None:
+def romanji_to_kana(word: str, dictionary: dict[str, str], tsu: str) -> str or None:
     """
-    Converts a string to hiragana, returns None if it isn't valid romanji
+    Converts a string to kana, using a specified dictionary returns None if it isn't valid romanji
+    :param tsu: Small tsu to use
+    :param dictionary: The dictionary to use
     :param word: The word to convert
     :return: The hiragana word
     """
-    i = 2 if word[0:2] == "> " else 0
-    hiragana_word = ""
+    i = 0
+    kana_word = ""
     while i < len(word):
         if i + 1 < len(word) and word[i] == word[i + 1]:
-            hiragana_word += "っ"  # Small tsu
+            kana_word += tsu  # Small tsu
             i += 1
         for j in range(min(3, len(word) - i), 0, -1):
-            if word[i:i + j] in romaji_to_hiragana_dict:
-                hiragana_word += romaji_to_hiragana_dict[word[i:i + j]]
+            if word[i:i + j] in dictionary:
+                kana_word += dictionary[word[i:i + j]]
                 i += j
                 break
         else:
             return None
 
-    return hiragana_word
+    return kana_word
 
 
-def hiragana_to_katakana(text: str) -> str:
+def romanji_to_hiragana(word) -> str or None:
+    return romanji_to_kana(word, romaji_to_hiragana_dict, 'っ')
+
+
+def romaji_to_katakana(word: str) -> str or None:
     """
-    Converts hiragana to katakana
-    :param text: The text to convert
-    :return: The converted text
+    Converts a string to katakana, returns None if it isn't valid romanji
+    :param word: The word to convert
+    :return: The katakana word
     """
+    kata = romanji_to_kana(word, romaji_to_katakana_dict, 'ッ')
+    if not kata:
+        return None
 
-    def to_katakana(char, next_char):
-        if char in set_a and next_char == 'あ' or \
-                char in set_e and (next_char == 'え' or next_char == 'い') or \
-                char in set_i and next_char == 'い' or \
-                char in set_o and (next_char == 'お' or next_char == 'う') or \
-                char in set_u and next_char == 'う' or \
-                char == 'え' and next_char == 'い' or \
-                char == 'お' and next_char == 'う':
+    def convert_choonpu(char, next_char):
+        if char in set_a and next_char == 'ア' or \
+                char in set_e and (next_char == 'エ' or next_char == 'イ') or \
+                char in set_i and next_char == 'イ' or \
+                char in set_o and (next_char == 'オ' or next_char == 'ウ') or \
+                char in set_u and next_char == 'ウ' or \
+                char == 'エ' and next_char == 'イ' or \
+                char == 'オ' and next_char == 'ウ':
             return 'ー'
-        return hiragana_to_katakana_dict[next_char]
+        return next_char
 
     katakana = ""
-    for i in range(len(text)-1, -1, -1):
+    for i in range(len(kata) - 1, -1, -1):
         if i == 0:
-            katakana = hiragana_to_katakana_dict[text[i]] + katakana
+            katakana = kata[i] + katakana
         else:
-            katakana = to_katakana(text[i - 1], text[i]) + katakana
+            katakana = convert_choonpu(kata[i - 1], kata[i]) + katakana
     return katakana
 
 
@@ -134,28 +145,44 @@ romaji_to_hiragana_dict: dict[str, str] = {
     'pya': 'ぴゃ', 'pyu': 'ぴゅ', 'pyo': 'ぴょ'
 }
 
-# Hiragana to Katakana dictionary
-hiragana_to_katakana_dict = {
-    'あ': 'ア', 'い': 'イ', 'う': 'ウ', 'え': 'エ', 'お': 'オ',
-    'か': 'カ', 'き': 'キ', 'く': 'ク', 'け': 'ケ', 'こ': 'コ',
-    'さ': 'サ', 'し': 'シ', 'す': 'ス', 'せ': 'セ', 'そ': 'ソ',
-    'た': 'タ', 'ち': 'チ', 'つ': 'ツ', 'て': 'テ', 'と': 'ト',
-    'な': 'ナ', 'に': 'ニ', 'ぬ': 'ヌ', 'ね': 'ネ', 'の': 'ノ',
-    'は': 'ハ', 'ひ': 'ヒ', 'ふ': 'フ', 'へ': 'ヘ', 'ほ': 'ホ',
-    'ま': 'マ', 'み': 'ミ', 'む': 'ム', 'め': 'メ', 'も': 'モ',
-    'や': 'ヤ', 'ゆ': 'ユ', 'よ': 'ヨ',
-    'ら': 'ラ', 'り': 'リ', 'る': 'ル', 'れ': 'レ', 'ろ': 'ロ',
-    'わ': 'ワ', 'を': 'ヲ', 'ん': 'ン',
-    'が': 'ガ', 'ぎ': 'ギ', 'ぐ': 'グ', 'げ': 'ゲ', 'ご': 'ゴ',
-    'ざ': 'ザ', 'じ': 'ジ', 'ず': 'ズ', 'ぜ': 'ゼ', 'ぞ': 'ゾ',
-    'だ': 'ダ', 'ぢ': 'ヂ', 'づ': 'ヅ', 'で': 'デ', 'ど': 'ド',
-    'ば': 'バ', 'び': 'ビ', 'ぶ': 'ブ', 'べ': 'ベ', 'ぼ': 'ボ',
-    'ぱ': 'パ', 'ぴ': 'ピ', 'ぷ': 'プ', 'ぺ': 'ペ', 'ぽ': 'ポ',
-    'ゃ': 'ャ', 'ゅ': 'ュ', 'ょ': 'ョ', 'っ': 'ッ'
+romaji_to_katakana_dict: dict[str, str] = {
+    'a': 'ア', 'i': 'イ', 'u': 'ウ', 'e': 'エ', 'o': 'オ',
+    'ka': 'カ', 'ki': 'キ', 'ku': 'ク', 'ke': 'ケ', 'ko': 'コ',
+    'sa': 'サ', 'shi': 'シ', 'su': 'ス', 'se': 'セ', 'so': 'ソ',
+    'ta': 'タ', 'chi': 'チ', 'tsu': 'ツ', 'te': 'テ', 'to': 'ト',
+    'na': 'ナ', 'ni': 'ニ', 'nu': 'ヌ', 'ne': 'ネ', 'no': 'ノ',
+    'ha': 'ハ', 'hi': 'ヒ', 'fu': 'フ', 'he': 'ヘ', 'ho': 'ホ',
+    'ma': 'マ', 'mi': 'ミ', 'mu': 'ム', 'me': 'メ', 'mo': 'モ',
+    'ya': 'ヤ', 'yu': 'ユ', 'yo': 'ヨ',
+    'ra': 'ラ', 'ri': 'リ', 'ru': 'ル', 're': 'レ', 'ro': 'ロ',
+    'wa': 'ワ', 'n': 'ン',
+    'ga': 'ガ', 'gi': 'ギ', 'gu': 'グ', 'ge': 'ゲ', 'go': 'ゴ',
+    'za': 'ザ', 'ji': 'ジ', 'zu': 'ズ', 'ze': 'ゼ', 'zo': 'ゾ',
+    'da': 'ダ', 'dzu': 'ヅ', 'de': 'デ', 'do': 'ド',
+    'ba': 'バ', 'bi': 'ビ', 'bu': 'ブ', 'be': 'ベ', 'bo': 'ボ',
+    'pa': 'パ', 'pi': 'ピ', 'pu': 'プ', 'pe': 'ペ', 'po': 'ポ',
+    'kya': 'キャ', 'kyu': 'キュ', 'kyo': 'キョ',
+    'sha': 'シャ', 'shu': 'シュ', 'sho': 'ショ',
+    'cha': 'チャ', 'chu': 'チュ', 'cho': 'チョ',
+    'nya': 'ニャ', 'nyu': 'ニュ', 'nyo': 'ニョ',
+    'hya': 'ヒャ', 'hyu': 'ヒュ', 'hyo': 'ヒョ',
+    'mya': 'ミャ', 'myu': 'ミュ', 'myo': 'ミョ',
+    'rya': 'リャ', 'ryu': 'リュ', 'ryo': 'リョ',
+    'gya': 'ギャ', 'gyu': 'ギュ', 'gyo': 'ギョ',
+    'ja': 'ジャ', 'ju': 'ジュ', 'jo': 'ジョ',
+    'dya': 'ヂャ', 'dyu': 'ヂュ', 'dyo': 'ヂョ',
+    'bya': 'ビャ', 'byu': 'ビュ', 'byo': 'ビョ',
+    'pya': 'ピャ', 'pyu': 'ピュ', 'pyo': 'ピョ',
+    'wi': 'ウィ', 'we': 'ウェ', 'wo': 'ウォ',
+    'va': 'ヴァ', 'vi': 'ヴィ', 'vu': 'ヴ', 've': 'ヴェ', 'vo': 'ヴォ',
+    'fa': 'ファ', 'fi': 'フィ', 'fe': 'フェ', 'fo': 'フォ',
+    'ti': 'ティ', 'tu': 'トゥ', 'di': 'ディ', 'du': 'ドゥ',
+    'je': 'ジェ', 'she': 'シェ', 'che': 'チェ',
+    'tsa': 'ツァ', 'tsi': 'ツィ', 'tse': 'ツェ', 'tso': 'ツォ'
 }
 
-set_a = {'あ', 'か', 'さ', 'た', 'な', 'は', 'ま', 'や', 'ら', 'わ', 'が', 'ざ', 'だ', 'ば', 'ぱ'}
-set_i = {'い', 'き', 'し', 'ち', 'に', 'ひ', 'み', 'り', 'ぎ', 'じ', 'ぢ', 'び', 'ぴ'}
-set_u = {'う', 'く', 'す', 'つ', 'ぬ', 'ふ', 'む', 'ゆ', 'る', 'ぐ', 'ず', 'づ', 'ぶ', 'ぷ'}
-set_e = {'え', 'け', 'せ', 'て', 'ね', 'へ', 'め', 'れ', 'げ', 'ぜ', 'で', 'べ', 'ぺ'}
-set_o = {'お', 'こ', 'そ', 'と', 'の', 'ほ', 'も', 'よ', 'ろ', 'ご', 'ぞ', 'ど', 'ぼ', 'ぽ'}
+set_a = {'ア', 'カ', 'サ', 'タ', 'ナ', 'ハ', 'マ', 'ヤ', 'ラ', 'ワ', 'ガ', 'ザ', 'ダ', 'バ', 'パ'}
+set_i = {'イ', 'キ', 'シ', 'チ', 'ニ', 'ヒ', 'ミ', 'リ', 'ギ', 'ジ', 'ヂ', 'ビ', 'ピ', 'ィ'}
+set_u = {'ウ', 'ク', 'ス', 'ツ', 'ヌ', 'フ', 'ム', 'ユ', 'ル', 'グ', 'ズ', 'ヅ', 'ブ', 'プ'}
+set_e = {'エ', 'ケ', 'セ', 'テ', 'ネ', 'ヘ', 'メ', 'レ', 'ゲ', 'ゼ', 'デ', 'ベ', 'ペ', 'ェ'}
+set_o = {'オ', 'コ', 'ソ', 'ト', 'ノ', 'ホ', 'モ', 'ヨ', 'ロ', 'ゴ', 'ゾ', 'ド', 'ボ', 'ポ', 'ォ'}
